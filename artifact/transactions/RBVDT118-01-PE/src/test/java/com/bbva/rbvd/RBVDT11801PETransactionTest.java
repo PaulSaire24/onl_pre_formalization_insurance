03254520cebe11afc66f33b75c00a9ba60b82197
Victor Moreno <victor.moreno.zapata@bbva.com>
2024-03-14 18:01:58 -0500

    feat: update tests

--- artifact/transactions/RBVDT118-01-PE/src/test/java/com/bbva/rbvd/RBVDT11801PETransactionTest.java
+++ artifact/transactions/RBVDT118-01-PE/src/test/java/com/bbva/rbvd/RBVDT11801PETransactionTest.java
@@ -1,28 +1,5 @@
 package com.bbva.rbvd;
 
-import com.bbva.elara.domain.transaction.Severity;
-import com.bbva.rbvd.dto.preformalization.PreformalizationDTO;
-import com.bbva.rbvd.lib.rbvd118.RBVDR118;
-import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.mockito.InjectMocks;
-import org.mockito.Mock;
-import org.mockito.runners.MockitoJUnitRunner;
-import org.slf4j.Logger;
-
-import static org.junit.Assert.assertEquals;
-import static org.mockito.Mockito.when;
-
-@RunWith(MockitoJUnitRunner.class)
 public class RBVDT11801PETransactionTest {
-
-    @Mock
-    private RBVDR118 rbvdR118;
-
-    @Mock
-    private Logger LOGGER;
-
-    @InjectMocks
-    private RBVDT11801PETransaction transaction;
-
+    // Todo here
 }
